8574049a124e90c906201703433ee985d9671c4d
Anisa Farcha <dailyfn93@gmail.com>
2026-03-16 21:45:24 +0700

    Create biodata.dart

---
+++ Lab_week2/biodata.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Anisa Farcha";
+  String nim = "43324101";
+  String major = "Computer Engineering Technology";
+
+  print("Name: $name");
+  print("Student ID: $nim");
+  print("Major: $major");
+}
